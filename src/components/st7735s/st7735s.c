9e0f47444fd725cbc2c9fd1d49ca1afa43e4843e
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2021-02-28 14:00:37 +0200

    st7735 - add license

--- src/components/st7735s/st7735s.c
+++ src/components/st7735s/st7735s.c
@@ -1,3 +1,20 @@
+/**
+ *     st7735 display library
+ *
+ *     Copyright (c) 2020 Vitaliy Nimych (Cvetaev) @ cvetaevvitaliy@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "st7735s.h"
 #include "st7735s_settings.h"
 #include "delay.h"
