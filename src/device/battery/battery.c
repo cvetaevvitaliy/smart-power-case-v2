e0791d9a7208071211cb1d6257fb4d7743a1ba54
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2021-02-28 14:12:17 +0200

    refactor service

--- src/device/battery/battery.c
+++ src/device/battery/battery.c
@@ -3,12 +3,54 @@
 #include "stm32f4xx.h"
 #include "cli.h"
 
-Battery_status_t Battery_status;
+extern I2C_HandleTypeDef hi2c1;
+
+static Battery_status_t Battery_status = {0};
+
+
+/**
+    Read a specified number of bytes over I2C at a given subAddress
+
+    @param subAddress is the 8-bit address of the data to be read
+           dest is the data buffer to be written to
+           count is the number of bytes to be read
+    @return true on success
+*/
+static int16_t BQ27441_i2cReadBytes(uint8_t DevAddress, uint8_t subAddress, uint8_t * dest, uint8_t count)
+{
+    if (HAL_I2C_Mem_Read(&hi2c1, (uint16_t)(DevAddress << 1), subAddress, 1, dest, count, 50) == HAL_OK)
+        return true;
+    else
+        return false;
+}
+
+/**
+    Write a specified number of bytes over I2C to a given subAddress
+
+    @param subAddress is the 8-bit address of the data to be written to
+           src is the data buffer to be written
+           count is the number of bytes to be written
+    @return true on success
+*/
+static int16_t BQ27441_i2cWriteBytes(uint8_t DevAddress, uint8_t subAddress, uint8_t * src, uint8_t count)
+{
+    if (HAL_I2C_Mem_Write(&hi2c1, (uint16_t)(DevAddress << 1), subAddress, 1, src, count, 50) == HAL_OK)
+        return true;
+    else
+        return false;
+}
+
+static BQ27441_ctx_t BQ27441 =
+        {
+            .read_reg = BQ27441_i2cReadBytes,
+            .write_reg = BQ27441_i2cWriteBytes,
+            .BQ27441_i2c_address = BQ72441_I2C_ADDRESS,
+        };
+
 
 void Battery_Init(void)
 {
-    BQ27441_init();
-
+    BQ27441_init(&BQ27441);
 }
 
 void Battery_LoopService(void)
@@ -17,6 +59,7 @@ void Battery_LoopService(void)
 
     if (HAL_GetTick() - tv_delay > 500)
     {
+        ULOG_TRACE("BQ27441 deviceType 0x%04X\n", BQ27441_deviceType());
         Battery_status.temperature = (float)BQ27441_temperature(BATTERY) / 100.0f;
         Battery_status.capacity = BQ27441_capacity(REMAIN_F);
         Battery_status.capacity_full = BQ27441_capacity(FULL_F); // FULL_F
